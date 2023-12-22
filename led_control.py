344358e12b930d9c93c6e33d25bba330af5a806e
calvindo95 <calvindo95@gmail.com>
2023-12-21 20:51:14 -0600

    Updates for multipcpu

--- led_control.py
+++ led_control.py
@@ -4,21 +4,20 @@ import board
 import neopixel
 
 class LedControl():
-    pixels = neopixel.NeoPixel(board.D18, 12,auto_write=False, brightness=0.2)
+    pixels = neopixel.NeoPixel(board.D18, 24,auto_write=False, brightness=0.2)
 
     def __init__(self):
         self.pixels.fill((0,0,0))
 
     def set_led(self, m_num_cpu, m_float):
-
         led_count = math.ceil(m_float/10 * 12/10)
         
         r_pix = math.floor(m_float * 255 / 100)
         g_pix = 255 - (math.floor(m_float * 255 / 100))
-
-        for i in range(led_count * m_num_cpu, led_count * m_num_cpu + 12):
-            self.pixels[i] = (r_pix,g_pix,0) 
-        for i in range(led_count * m_num_cpu, led_count * m_num_cpu + 12):
+        for i in range(12 * (m_num_cpu), led_count + 12 * (m_num_cpu)):
+            self.pixels[i] = (r_pix,g_pix,0)
+ 
+        for i in range(led_count + 12 * m_num_cpu, (m_num_cpu * 12)+12):
             self.pixels[i] = (0,0,0)
         
         self.pixels.show()
